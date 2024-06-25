a395b1e112e804479f0d118bae66c58d52a06c65
HolyTostadora <164009725+HolyTostadora@users.noreply.github.com>
2024-06-25 14:05:40 -0300

    script spawner anda al 100%

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -10,22 +10,28 @@ public class Spawner : MonoBehaviour
     public float intervaloSpawn;
     private int indiceObjeto;
     private int cantidadDeObjCreados;
+    private int contadorDeRepeticiones = 0;
+
 
     void Start()
     {
+        
         cantidadDeObjCreados = Random.Range(1,10);
         indiceObjeto = Random.Range(0, objetos.Length);
         Debug.Log(cantidadDeObjCreados);
-        for (int i = 0; i < cantidadDeObjCreados; i++)
-        {
-            spawnObject();
-        }
+        InvokeRepeating(nameof(spawnObject), 0, 1);
     }
 
     void spawnObject()
     {
+        if (contadorDeRepeticiones >= cantidadDeObjCreados)
+        {
+            CancelInvoke("spawnObject");
+            return;
+        }
         GameObject objectToSpawn = objetos[indiceObjeto];
         Instantiate(objectToSpawn, spawnPoint.position, spawnPoint.rotation);
+        contadorDeRepeticiones++;
     }
 }
 
